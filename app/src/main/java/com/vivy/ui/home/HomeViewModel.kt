929033be402408c0dbea734669f22777fda52977
Abanoub Wagdy <abanoubwagdywahib@gmail.com>
2019-08-08 20:47:06 +0200

    initial commit

---
+++ app/src/main/java/com/vivy/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.com.starter_project.ui.home
+
+import javax.inject.Inject
+import com.com.starter_project.ui.BaseViewModel
+
+class HomeViewModel @Inject constructor() : BaseViewModel() {
+}
---
+++ app/src/main/java/com/vivy/ui/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.com.starter_project.ui.main
+
+import javax.inject.Inject
+import com.com.starter_project.ui.BaseViewModel
+
+class MainViewModel @Inject constructor() : BaseViewModel() {
+
+
+}
